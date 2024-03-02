d61dcc3b2d40f03e4a16210bc793e57bcef03705
JoDoFa <JoDoFa>
2024-03-02 13:41:10 +0800

    added details

---
+++ hello.php
@@ -0,0 +1,27 @@
+    <html>
+         <header>
+            <title> Hello php </title>
+</header>
+<body>
+    <h1>
+        <?php
+
+$number = 12;
+
+if (is_int($number)) {
+    echo "$number is an integer.";
+} elseif (is_int($number)) {
+    echo "$number is not an integer.";  
+} elseif (is_string($number)) {
+    echo "$number is a String";
+} elseif (is_string($number)) {
+    echo "$number is not a String ";
+}
+
+
+
+    ?>
+    </h1>
+    </body>
+    </html>
+ 
